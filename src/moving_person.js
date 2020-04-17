9b83daa39da0a24a308b14bb159d8197aeeb228a
Michael_Scheer <macas.scheer@gmail.com>
2020-04-16 17:08:58 -0700

    build beginning draw/move functions

--- src/moving_person.js
+++ src/moving_person.js
@@ -8,30 +8,30 @@ function MovingPerson(obj) {
         this.vel = obj.vel;
         this.color = obj.color;
         this.age = obj.age;
+        this.ctx = obj.ctx;
         // type will determine color and velocity, will represent age
 }
-MovingPerson.prototype.draw = function(ctx){
-        ctx.fillStyle = this.color;
-
-        ctx.beginPath();
-        ctx.arc(
-                this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-        );
-        ctx.fill();
-        this.move(vel)
+MovingPerson.prototype.draw = function(pos = this.pos){
+        this.ctx.fillStyle = this.color;
+        this.ctx.beginPath();
+        this.ctx.arc(pos[0], pos[1], this.radius, 0, 2 * Math.PI, true);
+        this.ctx.fill();
+        this.move(pos)
 
 }
 
-MovingPerson.prototype.move = function (vel) {
+MovingPerson.prototype.move = async function (pos) {
         await this.sleepFunction(10);
-        this.asyncMove()
+        let newPos = [pos[0] + this.vel[0], pos[1] + this.vel[1]]
+        // this.asyncMove()
+        this.draw(newPos)
         
 }
 MovingPerson.prototype.sleepFunction = function (ms) {
         return new Promise(res => setTimeout(res, ms));
 };
 
-MovingPerson.prototype.asyncMove = function () {
-        return this.move(vel);
-};
+// MovingPerson.prototype.asyncMove = function () {
+//         return this.move(vel);
+// };
 module.exports = MovingPerson;
--- src/scenario.js
+++ src/scenario.js
@@ -3,7 +3,7 @@ const Locale = require("./locale.js")
 
 function Scenario(ctx, demoObj) {
     // this.city = obj.city;
-    this.population = deomoObj.population;
+    this.population = demoObj.population;
     this.ctx = ctx;
     // this.area = demoObj.area;
     // this.numRelig = demoObj.numrelig;
@@ -14,48 +14,33 @@ function Scenario(ctx, demoObj) {
     this.seniors = demoObj.population * demoObj.percentSeniors;
     this.popDensity = Math.floor(this.area / this.population);
     // this.size
-    this.minorObj = { radius: 5, color: "#2980B9", vel: 10, age: "1-12"}
-    this.teenObj = { radius: 15, color: "#27AE60", vel: 18, age: "13-17"};
-    this.adultObj = { radius: 20, color: "#BA4A00", vel: 23, age: "18-65"};
-    this.seniorObj = { radius: 22, color: "#707B7C", vel: 15, age: "66 and up"};
+    this.minorObj = { radius: 5, color: "#2980B9", vel: [-10,10], age: "1-12"}
+    this.teenObj = { radius: 15, color: "#27AE60", vel: [18,-18], age: "13-17"};
+    this.adultObj = { radius: 20, color: "#BA4A00", vel: [-23,23], age: "18-65"};
+    this.seniorObj = { radius: 22, color: "#707B7C", vel: [15, -15], age: "66 and up"};
 }
 
 Scenario.prototype.createPersons = function () {
-    this.createLoop(minor, this.minors)
-    this.createLoop(teen, this.teens)
-    this.createLoop(minor, this.adults)
-    this.createLoop(minor, this.seniors)
+    this.createLoop("minor", this.minors)
+    this.createLoop("teen", this.teens)
+    this.createLoop("adult", this.adults)
+    this.createLoop("senior", this.seniors)
 }
 
 Scenario.prototype.createLoop = function (ageGroup, n) {
-    let type = "sick";
-    let age;
-    let radius;
-    let vel;
-    let color;
     switch (ageGroup) {
-        case minor:
-            radius = this.minorObj.radius;
-            vel = [minorObj.vel, minorObj.vel];
-            color = this.minorObj.color;
-            age = this.minorObj.age;
+        case "minor":
+            obj = this.minorObj;
             break;
-        case teen:
-            radius = this.teenObj.radius;
-            vel = [teenObj.vel, teenObj.vel];
-            color = this.teenObj.color;
-            age = this.teenObj.age;
-        case adult:
-            radius = this.adultObj.radius;
-            vel = [adultObj.vel, adultObj.vel];
-            color = this.adultObj.color;
-            age = this.adultObj.age;
-        case senior:
-            radius = this.seniorObj.radius;
-            vel = [seniorObj.vel, seniorObj.vel];
-            color = this.seniorObj.color;
-            age = this.seniorObj.age;
+        case "teen":
+            obj = this.teenObj;
+        case "adult":
+            obj = this.adultObj;
+        case "senior":
+            obj = this.seniorObj;
     }
+    let type = "sick";
+    let {radius, vel, color, age} = obj
     for (let i = 0; i < n; i++) {
         let pos1 = Math.floor(Math.random(2) * 100 * Math.random(10) * 50)
         let pos2 = Math.floor(Math.random(2) * 100 * Math.random(10) * 50)
@@ -68,9 +53,10 @@ Scenario.prototype.createLoop = function (ageGroup, n) {
             pos,
             vel,
             color,
-            age
+            age, 
+            ctx : this.ctx
         })
-        person.draw(this.ctx)
+        person.draw()
     }
 }
 Scenario.prototype.requestData = function () {
