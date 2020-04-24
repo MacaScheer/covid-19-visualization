5ddb7eed5a3469c18e1aa4ddec14057cd6afccb3
Michael_Scheer <macas.scheer@gmail.com>
2020-04-24 11:44:17 -0700

    start differing logic for disease spread to the different child classes of moving person

--- src/adult.js
+++ src/adult.js
@@ -21,18 +21,18 @@ Util.inherits(Adult, MovingPerson);
 Adult.prototype.collideWith = function collideWith(otherPerson) {
     // if this.infected, spread to otherPerson
     // if otherPerson.infect and !this.infected spread to this.Adult
-    debugger
-    if (this.type === "infected" || otherPerson.type === "infected") {
+    // debugger
+    if (this.type === "infect" && otherPerson.type === "infected") {
+        // do nothing => both are already infected...but what would actually happen?
+    }
+    else if (this.type === "infected" || otherPerson.type === "infected") {
         this.type = "infected";
         otherPerson.type = "infected";
+        // debugger
         // will we need to re-instatiate both, removing them first, so they will be colored correctly?
         // also, should there be an incubation period? Where color changes (or not) and then only changes to the infected "green" after the incubation?
+        // this will change for the different age groups.
     }
-    // if (otherPerson instanceof MovingPerson) {
-
-
-    //     return true;
-    // }
 }
 
 
