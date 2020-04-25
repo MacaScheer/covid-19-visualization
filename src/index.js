3d94efd7f5354d1c3b1a258f55198058223ed0aa
Michael_Scheer <macas.scheer@gmail.com>
2020-04-24 17:59:09 -0700

    adding new components

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const Menu = require("./menu.js")
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("canvas-space");
     const ctx = canvas.getContext("2d");
-    const graphCanvas = document.getElementById("demo-graph")
-    const graphCtx = graphCanvas.getContext("2d")
+    // const graphCanvas = document.getElementById("demo-graph")
+    // const graphCtx = graphCanvas.getContext("2d")
     const menu = new Menu()
     // let [demObj, demSick] = await menu.requestParameters()
     let demoObj = {
